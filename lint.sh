11d2f5de9d3b6710d4aae1e1b1fcf34ac731ca6b
Ansible Git Repo Automation <ansible@shakey.org>
2024-07-15 14:05:42 +0000

    Failing local lint example

---
+++ lint.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/usr/bin/ansible-lint -R -r ./code -f json
---
+++ rules/DeprecatedVariableRule.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+from ansiblelint.rules import AnsibleLintRule
+
+class DeprecatedVariableRule(AnsibleLintRule):
+    """Deprecated variable declarations."""
+    id = 'LSHAKE001'
+    description = 'Check for lines that have old style variable ' + 'declarations'
+    tags = { 'deprecations' }
+    severity = "HIGH"
+    version_added = "6.14.3"
+
+    def match(self, line: str) -> Union[bool, str]:
+        results = []
+        if '##' in line:
+            return 'old style variable names'
+        else:
+            return False
